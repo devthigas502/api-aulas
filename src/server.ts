6fe5bb4e47e4b6a19fdcc83d7a3e75237b018a89
Thiago Henrique A. Ribeiro <thiagodev502@gmail.com>
2025-08-27 00:20:56 -0300

    fix: corrige importação ECMAScript e tipagem para build com nodenext

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
-import { server } from './app'
+import { server } from './app.js'
 
 
 server.listen({ port: 8080 }, (err, address) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  console.log(`Server listening at ${address}`)
+})
+server.listen({ port: 8080 }, (err: unknown, address: string) => {
   if (err) {
     console.error(err)
     process.exit(1)
